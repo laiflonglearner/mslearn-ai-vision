fbfa4e51690b1f711578a071132f7603af3bc436
nadiarizkyhairunnisa <nadiarizkyh.dev@gmail.com>
2024-03-13 23:43:28 +0700

    add namespaces, authentication, and specify people feature

--- Labfiles/04-face/Python/computer-vision/detect-people.py
+++ Labfiles/04-face/Python/computer-vision/detect-people.py
@@ -8,7 +8,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 # Import namespaces
-
+import azure.ai.vision as sdk
 
 
 def main():
@@ -26,7 +26,7 @@ def main():
             image_file = sys.argv[1]
 
         # Authenticate Azure AI Vision client
-
+        cv_client = sdk.VisionServiceOptions(ai_endpoint, ai_key)
         
         # Analyze image
         AnalyzeImage(image_file, cv_client)
@@ -39,7 +39,11 @@ def AnalyzeImage(image_file, cv_client):
     print('\nAnalyzing', image_file)
 
     # Specify features to be retrieved (PEOPLE)
-
+    analysis_options = sdk.ImageAnalysisOptions()
+        
+    features = analysis_options.features = (
+        sdk.ImageAnalysisFeature.PEOPLE
+    )    
 
     # Get image analysis
 
